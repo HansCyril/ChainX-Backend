8b117ceeee74ab6dbad0ef8fa800339fe0871ae3
hanscyril <hans.eder@urios.edu.ph>
2026-04-26 12:31:54 +0800

    Fix Vercel deployment by persisting bootstrap/cache directory and mapping APP_SERVICES_CACHE to /tmp

--- api/index.php
+++ api/index.php
@@ -1,13 +1,2 @@
 <?php
-
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (\Throwable $e) {
-    header('HTTP/1.1 500 Internal Server Error');
-    echo "<h1>500 Internal Server Error</h1>";
-    echo "<pre>";
-    echo "Message: " . $e->getMessage() . "\n\n";
-    echo "Stack Trace:\n" . $e->getTraceAsString();
-    echo "</pre>";
-    exit;
-}
+require __DIR__ . '/../public/index.php';
