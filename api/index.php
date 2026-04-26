f06cc8763c83d4b70dc0d57a3ab87ecf9e563e71
hanscyril <hans.eder@urios.edu.ph>
2026-04-26 14:14:46 +0800

    Add exception handler to Vercel entrypoint to trace 500 error causes in production

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo '<pre>Caught Exception in Vercel API Bridge: ' . (string) $e . '</pre>';
+}
